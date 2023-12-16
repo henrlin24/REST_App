8a422139bc6b23e3f09941fe86a3d75d285d4e65
henrlin24 <henrlin@umich.edu>
2023-12-16 15:37:34 -0500

    Basic backend interaction with React set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+/* React Hooks
+    only usable within function components, cannot use within class components (now somewhat obsolete)
+    have to be called in the same order every time - aka NO conditionals, functions, or loops
+      can only be at top level of functional component
+
+*/
+
+// useState - allows for the maintaining of state, and a corresponding update function
+// useEffect - similar to an "onAction" function, allows for additional effects after something is changed
+
 
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
