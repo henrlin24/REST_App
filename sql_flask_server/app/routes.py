24cf8e9cfeced5d846a4f352ade09ce57874e130
henrlin24 <henrlin@umich.edu>
2023-12-17 23:06:44 -0500

    Created base template and index template inheriting from base

--- sql_flask_server/app/routes.py
+++ sql_flask_server/app/routes.py
@@ -1,4 +1,5 @@
 # module to handle different URLs for the application
+from flask import render_template
 from app import app
 
 # view functions, which are mapped to one or more route URLs, and tell Flask what logic to respond with for different URLs
@@ -7,4 +8,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, world!"
+  # mock user
+  user = {'username': 'Henry'}
+
+  # mock posts
+  posts = [
+    {
+      'author' : {'username': 'John'},
+      'body' : 'Beautiful day! Somewhere, at least.'
+    },
+    {
+      'author' : {'username': 'Doe'},
+      'body': 'I\'m at somewhere!'
+    }
+  ]
+
+  # returns a rendered complete HTML page using the template provided, using the jinja template engine bundled with Flask
+  return render_template('index.html', title='Home', user=user, posts=posts)
