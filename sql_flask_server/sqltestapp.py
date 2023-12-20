f635dedf3aa3ebf381ef6e90e6a6925ce2ec73ff
henrlin24 <henrlin@umich.edu>
2023-12-19 20:25:57 -0500

    Flask Pt4.3
    -shell context function added, for use when running Python interpreter with 'flask shell' command for easy testing with pre-imported symbols

--- sql_flask_server/sqltestapp.py
+++ sql_flask_server/sqltestapp.py
@@ -1,1 +1,11 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+# for use with 'flask shell'
+# decorator registers function as shell context function, which will then run when 'flask shell' is run
+@app.shell_context_processor
+def make_shell_context():
+  # returns dictionary of reference name within shell and associated item
+  return {'sa': sa, 'so': so, 'app': app, 'db': db, 'User': User, 'Post': Post}
