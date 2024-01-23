456924c2516d0642d6505aacb74621020b28dd6c
henrlin24 <henrlin@umich.edu>
2024-01-22 19:40:46 -0500

    Flask 5.2 User register

--- sql_flask_server/app/forms.py
+++ sql_flask_server/app/forms.py
@@ -1,9 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   password = PasswordField('Password', validators=[DataRequired()])
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])        # ensures data entered matches structure of email address
+  password = PasswordField('Password', validators=[DataRequired()])
+  # reconfirms password, checking it matches with initial password entry
+  password2 = PasswordField(
+    'Re-confirm password', validators=[DataRequired(), EqualTo(password)])
+  submit = SubmitField('Register')
+
+  # validators of the pattern validate_<field_name>, WTForms recognizes them as custom validators and runs alongside stock validators
+  def validate_username(self, username):
+    # uses scalar() instead of scalars(), as its safe to assume only one result if any (None otherwise)
+    user = db.session.scalar(sa.select(User).where(
+      User.username == username.data
+    ))
+    if user is not None:
+      raise ValidationError('The username {} is taken. Try a different username!'.format(username.data))
+  
+  def validate_email(self, email):
+    email = db.session.scalar(sa.select(User).where(
+      User.email == email.data
+    ))
+    if email is not None:
+      raise ValidationError('The email {} is already in use. Try registering with a different email!'.format(email.data))
--- sql_flask_server/app/routes.py
+++ sql_flask_server/app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required # current_user can be referenced at any time during request handling to obtain user object representing request client
 import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from urllib.parse import urlsplit
 
@@ -67,8 +67,24 @@ def login():
     return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
 
-# logout function
+# logout view function
 @app.route('/logout')
 def logout():
   logout_user()
   return redirect(url_for('index'))
+
+# register view function
+@app.route('/register')
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  
+  form = RegistrationForm()
+  if form.validate_on_submit:
+    user = User(username=form.username.data, email=form.username.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations {}, you are now a registered user! Have fun!'.format(form.username.data))
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Register', form=form)
